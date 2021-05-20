1bab5c1d7a59d7f99dd5dc69235d2a10e0fa9fbb
Martin Labryga <marcin@labryga.com>
2021-05-20 03:05:26 +0200

    build script by cleaning dangling images extended

--- django/build_image.sh
+++ django/build_image.sh
@@ -4,3 +4,5 @@ docker image build -t django_alpine:3.2.3 \
   --build-arg host_user_name=$USER \
   --build-arg host_user_id=$(id -u) \
   -f ./django.development.Dockerfile .
+
+docker rmi -f $(docker images -f dangling=true -q)
