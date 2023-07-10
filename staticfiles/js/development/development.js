9145f986f395e5f12d7321ddb4bf24928dc16576
Martin Labryga <martin@labryga.com>
2023-07-10 22:11:00 +0200

    animation objects added

--- staticfiles/js/development/development.js
+++ staticfiles/js/development/development.js
@@ -7,35 +7,25 @@ const canvas_height = canvas.offsetHeight;
 context.lineWidth = 2;
 context.strokeStyle = '#FAD807';
 
-// let i = 1;
-
-// function lineAnimation () {
-//   if (i>700) {
-//     cancelAnimationFrame(lineAnimation);
-//   } else {
-//     i += 2;
-//     context.clearRect(0,0,canvas.width,canvas.height);
-//     context.beginPath();
-//     context.moveTo(0,0);
-//     context.lineTo(i+10,i+10);
-//     context.stroke();
-//     let myReq = requestAnimationFrame(lineAnimation);
-//   }
-// }
-//
-// lineAnimation();
-
+let itemsArray = [];
 let division_factor = canvas_width/10;
+let offset_factor = division_factor/2
+
 for (d=division_factor; d<canvas_width; d+=division_factor) {
-
-  context.beginPath();
-  context.moveTo(0, d);
-  context.lineTo(d,0);
-  context.stroke();
-
-  context.beginPath();
-  context.moveTo(d, canvas_height);
-  context.lineTo(canvas_width,d);
-  context.stroke();
-
+  itemsArray.push(new lineObjects(d));  
 }
+
+function lineObjects (d) {
+  this.division_factor = d;
+  this.moveToX = (canvas_width - this.division_factor) + offset_factor;
+  this.lineToY = (canvas_height - this.division_factor) + offset_factor;
+  this.draw = function() {
+
+    context.beginPath();
+    context.moveTo(this.moveToX, 0);
+    context.lineTo(0, this.lineToY);
+    context.stroke();
+    };
+}
+
+itemsArray.forEach(element => element.draw());
