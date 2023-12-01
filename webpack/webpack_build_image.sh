e1b576055ea997eb3203914f337ebbc1c657c184
Martin Labryga <martin@labryga.com>
2023-12-01 22:24:42 +0100

    webpack removed

--- webpack/webpack_build_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker image build -t webpack_5:node_18.14.2-alpine3.17 \
-  --build-arg host_user_name=$USER \
-  --build-arg host_user_id=$(id -u) \
-  -f ./webpack.Dockerfile .
-
-docker rmi -f $(docker images -f dangling=true -q)
